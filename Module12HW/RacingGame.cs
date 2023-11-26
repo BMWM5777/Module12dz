6681f35cd56026d48b836bad5fe663950d8e4faf
mrdil <mrdil@BMW>
2023-11-26 22:52:36 +0500

    Добавьте файлы проекта.

---
+++ Module12HW/RacingGame.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module12HW
+{
+    class RacingGame
+    {
+        public event Car.RaceEventHandler StartRaceEvent; 
+
+        public delegate void FinishRaceDelegate(string winner);
+        public event FinishRaceDelegate FinishRaceEvent;
+
+        public void StartRace()
+        {
+            Console.WriteLine("Гонка началась!");
+            StartRaceEvent?.Invoke("Гонка началась!");
+        }
+
+        public void FinishRace(string winner)
+        {
+            Console.WriteLine($"Гонка окончена! Победитель: {winner}");
+            FinishRaceEvent?.Invoke(winner);
+        }
+    }
+}
